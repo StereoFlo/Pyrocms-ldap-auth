89ff7bd8f69b3534d2d57ebfcb958d47d1b6aed4
StereoFlo <stereoflo@stereoflo.ru>
2015-08-24 13:06:03 +0300

    Update adldap.php

--- auth/config/adldap.php
+++ auth/config/adldap.php
@@ -2,9 +2,9 @@
 
 $config['account_suffix']	= '@megafon-retail.ru';
 $config['base_dn']		= 'DC=megafon-retail,DC=ru';
-$config['domain_controllers']	= array ('10.183.248.2');
-$config['ad_username']		= 'e.bronnikov';
-$config['ad_password']		= 'Az160259';
+$config['domain_controllers']	= array ('');
+$config['ad_username']		= '';
+$config['ad_password']		= '';
 $config['real_primarygroup']	= true;
 $config['use_ssl']		= false;
 $config['use_tls'] 		= false;
